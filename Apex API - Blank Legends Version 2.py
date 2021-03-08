6a48659507d45a12ad70c00bed6156dec9fa2550
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-08 19:47:29 +0000

    Update Apex API - Blank Legends Version 2.py

--- Apex API - Blank Legends Version 2.py
+++ Apex API - Blank Legends Version 2.py
@@ -1,51 +1,68 @@
 import requests
 import json
 
-APIKey_file = open("C:/Users/jakeg/Documents/Apex.txt", "rt")
+class Legend:
+	def __init__(self, name, kills, percentile):
+		self.name = name
+		self.kills = kills
+		self.percentile = percentile
+	def getKills(self):
+		return self.kills
+	def getName(self):
+		return self.name
+	def getPercentile(self):
+		return self.percentile
+
+def getLegendKills(legend):
+	return legend.getKills()
+
+#setup
+APIKey_file = open("Apex.txt", "rt")
 APIKey = APIKey_file.read()
 
-url = 'https://public-api.tracker.gg/v2/apex/standard/profile/'
+all_legend_names = ["Bloodhound", "Gibraltar", "Lifeline", "Pathfinder", "Wraith", "Bangalore", "Caustic", "Mirage", 
+"Octane", "Wattson", "Crypto", "Revenant", "Loba", "Rampart", "Horizon", "Fuse"]
 
-player_name = 'majorquazar'
+url = 'https://public-api.tracker.gg/v2/apex/standard/profile/origin/'
 
-platform_name = 'origin'
+player_name = 'MajorQuazar'
 
 segment_type = 'legend'
 
-full_url = url + platform_name + "/" + player_name + "/segments" + "/" + segment_type
+full_url = url + player_name + "/segments" + "/" + segment_type
 
 payload = {}
 
 headers = {'TRN-Api-Key': APIKey}
 
+legend_data = list();
+
+#end setup
+
+
+#HTTP request to tracker.gg api
 response = requests.request("GET", full_url, headers=headers, data=payload)
-
 player_data = response.json()
+#process reponse data
+for legend in all_legend_names:
+	#set default kills to 0
+	kills = 0;
+	percentile = 0
+	for item in player_data["data"]:
+		#if legend name matches a player data legend name then try to get kill data
+		if item.get("metadata").get("name") == legend:
+			#if kill data is available then set value of kills
+			if item.get("stats") and item.get("stats").get("kills") and item.get("stats").get("kills").get("value"):
+				kills = int(item["stats"]["kills"]["value"])
+				percentile = int(item["stats"]["kills"]["percentile"])
+	#initialise Legend object for each legend using name and kills as parameters.
+	legend_data.append(Legend(legend, kills, percentile))
+	pass
 
-legend_names = ["Bloodhound", "Gibraltar", "Lifeline", "Pathfinder", "Wraith", "Bangalore", "Caustic", "Mirage", 
-"Octane", "Wattson", "Crypto", "Revenant", "Loba", "Rampart", "Horizon", "Fuse"]
-
-API_legend_name = []
-
-for name in player_data["data"]:
-	name = name["metadata"]["name"]
-	API_legend_name.append(name)
-
-print(legend_names)
-print(API_legend_name)
-
-def list_contains(List1, List2):
-    check = False
-    for x in List1: 
-        for y in List2: 
-            if x == y: 
-                check = True
-                return check
-    return check
-
-print(list_contains(legend_names,API_legend_name))
-
-for legend in legend_names:
-	if list_contains(legend_names,API_legend_name) == True:
-		print(legend)
-#str(int(legend_stats["stats"]["kills"]["value"]))
+#sort based by kill count (descending)
+legend_data.sort(key= getLegendKills, reverse = True)
+#print name with formatting
+print("-----\r" + player_name + "\r-----")
+#print all legend data
+for legend in legend_data:
+	print(str(legend.getKills()) + " kills with " + legend.getName())
