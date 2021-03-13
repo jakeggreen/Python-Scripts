c2434d450d011c55ca96257255d9d3fd3e7077dc
jakeggreen <80248380+jakeggreen@users.noreply.github.com>
2021-03-13 20:45:22 +0000

    Update Apex API - Sessions.py

--- Apex API - Sessions.py
+++ Apex API - Sessions.py
@@ -22,17 +22,19 @@ class Match:
 	def getRank(self):
 		return self.rankscore
 
+def getPlayerName(player):
+	return Match.getPlayer()
+
 #setup
 
 #pull in API key from text file
-APIKey_file = open("Apex.txt", "rt")
+APIKey_file = open('Apex.txt', 'rt')
 APIKey = APIKey_file.read()
 
 #get list of player names from separate text file
-# player_name_file = open("Players.txt", "rt")
-# players = player_name_file.read()
-# player_names = players.split("\n")
-player_names = {'MajorQuazar'}
+player_name_file = open('Players.txt', 'rt')
+players = player_name_file.read()
+player_names = players.split('\n')
 
 url = 'https://public-api.tracker.gg/v2/apex/standard/profile/origin/'
 
@@ -44,27 +46,25 @@ session_data = list();
 
 #end setup
 
-#check to see if player data is available
-try:
-	for player in player_names:
-		full_url = url + player + "/sessions"
-		response = requests.request("GET", full_url, headers=headers, data=payload)
-		player_data = response.json()
-		player = player
+for player in player_names:
+	full_url = url + player + '/sessions'
+	response = requests.request('GET', full_url, headers=headers, data=payload)
+	player_data = response.json()
+	player = player
+	#check to see if player data is available
+	if player_data.get('data') and player_data.get('data').get('items'):
 		#get the start and end dates for matches
-		for dates in player_data.get("data").get("items"):
-			startdate = datetime.datetime.strptime(dates["metadata"]["startDate"]["value"],'%Y-%m-%dT%H:%M:%S.%fZ')
-			enddate = datetime.datetime.strptime(dates["metadata"]["endDate"]["value"],'%Y-%m-%dT%H:%M:%S.%fZ')
-			#for each match get the legend used and the rank value
-			for matches in dates.get("matches"):
-				legend = matches["metadata"]["character"]["displayValue"]
-				rankscore = matches["stats"]["rankScore"]["value"]
+		for dates in player_data.get('data').get('items'):
+			startdate = datetime.datetime.strptime(dates['metadata']['startDate']['value'],'%Y-%m-%dT%H:%M:%S.%fZ')
+			enddate = datetime.datetime.strptime(dates['metadata']['endDate']['value'],'%Y-%m-%dT%H:%M:%S.%fZ')
+			#for each match get the legend used and the ending rank score
+			for matches in dates.get('matches'):
+				legend = matches['metadata']['character']['displayValue']
+				rankscore = matches['stats']['rankScore']['value']
 				session_data.append(Match(player, startdate, enddate, legend, rankscore));
-				# print(Match(player, startdate, enddate, legend, rankscore))
-except KeyError:
-	pass
-			
-for player in session_data:
-	# print("-----\r" + Match.getPlayer(player) + "\r-----")
-	print(Match.getPlayer(player) + " Started: " + str(Match.getStart(player)) + ", Ended: " 
-	+ str(Match.getEnd(player)) + ", Played With: " + str(Match.getLegend(player)) + ", Rank: " + str(Match.getRank(player)))
+
+
+for match in session_data:
+	# print('-----\r' + Match.getPlayer(match) + '\r-----')
+	print(Match.getPlayer(match) + ' - Start: ' + str(Match.getStart(match)) + ', End: ' 
+	+ str(Match.getEnd(match)) + ', Played With: ' + str(Match.getLegend(match)) + ', Rank: ' + str(Match.getRank(match)))
